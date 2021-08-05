81c34cf68cfb730414580cea9890ae1535f9d12e
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-05 13:18:28 -0400

    wip

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('web 44 rocks!')
+require('dotenv').config() //this allows to 'inject' fake environment variables
+
+const { PORT } = require('./config')
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>Web 44 Is Awesome!</h1>
+    `)
+})
+
+server.use('*', (req, res) => {
+    res.json({
+        message: 'web 44 is awesome indeed'
+    })
+})
+
+server.listen(PORT, () => { //heroku machine uses a different port
+    console.log(`listening on ${PORT}`)
+})
